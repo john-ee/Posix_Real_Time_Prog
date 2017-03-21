e4b4be7d6fce49a76dbe0c5dbe36578f33e8aa2a
Hill John-Nathan <john.nat.hill@gmail.com>
2017-03-21 16:12:25 +0100

    Je vais reprendre la comportement de ex1.c dans les processus

--- ex2.c
+++ ex2.c
@@ -16,21 +16,48 @@
 #define COUNT_LIMIT 30
 
 int pid;
-
+int k_final = 0;
 
 
 int main(int argc, char const *argv[]) {
 
+  int i = 9;
+
   struct timespec ref;
   clock_gettime(CLOCK_REALTIME, &ref);
   double t = (double)ref.tv_sec*1e9 + (double)ref.tv_nsec;
-  int k = 0;
+  int k_final = 0;
   while (((double)ref.tv_sec*1e9 + (double)ref.tv_nsec)<t+1e6) {
     clock_gettime(CLOCK_REALTIME, &ref);
-    k++;
+    k_final++;
   }
 
-  printf("k = %d\n", k);
+  int k = 0;
+  while (i>0) {
+    struct timespec ref;
+    clock_gettime(CLOCK_REALTIME, &ref);
+    double t = (double)ref.tv_sec*1e9 + (double)ref.tv_nsec;
+    k = 0;
+    while (((double)ref.tv_sec*1e9 + (double)ref.tv_nsec)<t+1e6) {
+      clock_gettime(CLOCK_REALTIME, &ref);
+      k++;
+    }
+    //printf("%d\n",k);
+    if (k<k_final)
+      k_final = k;
+    i--;
+  }
+
+  struct sched_param father_params;
+  int ret;
+  father_params.sched_priority = PRIORITY_F;
+  ret = sched_setscheduler(getpid(), SCHED_FIFO, &father_params);
+  if (ret == -1) {
+    perror("sched_setscheduler father");
+    exit(1);
+  }
+
+  printf("k = %d\n", k_final);
 
   if ( (pid=fork()) == -1 ) {
     perror("fork");
@@ -39,26 +66,19 @@ int main(int argc, char const *argv[]) {
 
 	if(pid) {
     // FATHER
-    struct sched_param father_params;
+    struct sched_param child_params;
     int ret;
-    father_params.sched_priority = PRIORITY_F;
-    ret = sched_setscheduler(getpid(), SCHED_FIFO, &father_params);
+    child_params.sched_priority = PRIORITY_C;
+    ret = sched_setscheduler(pid, SCHED_FIFO, &child_params);
     if (ret == -1) {
-      perror("sched_setscheduler father");
+      perror("sched_setscheduler child");
       exit(1);
     }
   }
 
   else {
     // CHILD
-    struct sched_param child_params;
-    int ret;
-    child_params.sched_priority = PRIORITY_C;
-    ret = sched_setscheduler(getpid(), SCHED_FIFO, &child_params);
-    if (ret == -1) {
-      perror("sched_setscheduler child");
-      exit(1);
-    }
+
   }
 
 
